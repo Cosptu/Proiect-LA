177ac380520b0fd34f740e91b803756bd864b06b
Costel Spinu <cosptu@gmail.com>
2021-07-03 09:03:47 +0300

    lastup

--- proiect.js
+++ proiect.js
@@ -1,6 +1,27 @@
 
-  var  likeButton = document.querySelector('.like-button')
+ /* var  likeButton = document.querySelector('.like-button')
   likeButton.addEventListener('click', () => {
     likeButton.classList.toggle('liked')
 })
+*/
+//select classes and id's
+const likeButton = document.querySelector(".likeBtn");
+let likeIcon = document.querySelector("#icon");
+let count = document.querySelector("#count");
 
+//button clicked
+let clicked = false
+
+likeButton.addEventListener("click", () => {
+  if (!clicked) {
+      clicked = true
+      likeIcon.innerHTML= `<i class="far fa-thumbs-up"></i> `
+      count.textContent++
+    }
+    else {
+      clicked = false
+      likeIcon.innerHTML= `<i class="far fa-thumbs-up"></i> `
+      count.textContent--
+
+    }
+});
