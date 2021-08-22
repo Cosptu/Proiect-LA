e9f5ac4214cf19e1a95cf1abf6c88c5031120f83
Costel Spinu <cosptu@gmail.com>
2021-08-22 13:28:59 +0300

    fix like button ok

--- proiect.js
+++ proiect.js
@@ -1,73 +1,54 @@
 
- /*var  likeButton = document.querySelector('likeBtn')
-  likeButton.addEventListener('click', () => {
-    console.log('test');
-    likeButton.classList.toggle('liked')
-})*/
-
-//select classes and id's
-//comm for modified version
-const likeButton = document.getElementsByClassName("likeBtn");
-//let likeIcon = likeButton.firstElementChild;
-//let likeIcon = document.getElementById("icon");
-//let count = document.getElementsByClassName("count");
-
+//access DOM
+var likeButton = document.getElementsByClassName("likeBtn");
 //button clicked
 let clicked = false
 
-//likeButton.addEventListener("click", function(){
+//add event on click 
   for (var i = 0 ; i < likeButton.length; i++) {
     let likeIcon = likeButton[i].firstElementChild;
-   // let count = likeButton[i];
-    //count.getElementById('count');
     likeButton[i].addEventListener('click', function(){
     console.log(likeIcon)
-    //console.log(count)
     likeIcon.classList.add('buttonclicked');
-    //count.innerHTML='test'
+
 
     }) ; 
  };
  
- 
-  
- 
- // if (!clicked) {
-    //console.log('test');
-  //    clicked = true;
-  //    likeIcon.innerHTML= `<i class="far fa-thumbs-up"></i> `
-   //   count.textContent++
-    //}
-  //  else {
-   //   clicked = false;
-   //   likeIcon.innerHTML= `<i class="far fa-thumbs-up"></i> `
-   //   count.textContent--
-
-  //  });
-
-
-//html button
-//<button class = likeBtn>
-  //<span id="icon"><i class="far fa-thumbs-up"></i></span>
-  //<span id="count">0</span> Likes
-  //</button>
-
-/*const likeButton =  document.getElementsByClassName(".likeBtn");
-let likeIcon = document.getElementById("#icon");
-let count = document.getElementById("#count");
-
-let clicked = false
-
-likeButton.addEventListener("click", () => {
-  if (!clicked) {
-      clicked = true
-      likeIcon.innerHTML= `<i class="far fa-thumbs-up"></i> `
-      count.textContent++
+ // se inroseste dar nu se intampla reversul la click 2
+ // <button onclick="myFunction()" class = likeBtn>
+ /* var butt = document.getElementById('icon')
+        function myFunction(){
+    if (butt.style.color == 'red'){
+        butt.style.color = 'grey'
     }
-    else {
-      clicked = false
-      likeIcon.innerHTML= `<i class="far fa-thumbs-up"></i> `
-      count.textContent--
+    else butt.style.color = 'red'
+}
+  */
 
-    }
-});*/
+// counting likes - in consola numara in site nu / to fix
+
+  /*  let count = 0;
+    function myFunction() {
+    count+=1;
+    document.getElementById("counts").innerHTML = count;
+    console.log(count)
+      }
+*/
+const likeBtn = document.querySelectorAll(".like__btn");
+likeBtn.forEach(function (item) {
+    item.addEventListener("click", (event) => {
+        let likeIcon = item.querySelector(".icon");
+        let count = item.querySelector(".count");
+        if (+count.textContent == 0) {
+            clicked = true;
+            likeIcon.innerHTML = `<i class="fas fa-thumbs-up"></i>`;
+            count.textContent++;
+        } else {
+            clicked = false;
+            likeIcon.innerHTML = `<i class="far fa-thumbs-up"></i>`;
+            count.textContent--;
+        }
+    });
+})
+
