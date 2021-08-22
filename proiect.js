1671ee2e8410fc0ce49361bd15704a2354f23dd7
Costel Spinu <cosptu@gmail.com>
2021-08-22 14:23:59 +0300

    remove borders

--- proiect.js
+++ proiect.js
@@ -1,40 +1,4 @@
 
-//access DOM
-var likeButton = document.getElementsByClassName("likeBtn");
-//button clicked
-let clicked = false
-
-//add event on click 
-  for (var i = 0 ; i < likeButton.length; i++) {
-    let likeIcon = likeButton[i].firstElementChild;
-    likeButton[i].addEventListener('click', function(){
-    console.log(likeIcon)
-    likeIcon.classList.add('buttonclicked');
-
-
-    }) ; 
- };
- 
- // se inroseste dar nu se intampla reversul la click 2
- // <button onclick="myFunction()" class = likeBtn>
- /* var butt = document.getElementById('icon')
-        function myFunction(){
-    if (butt.style.color == 'red'){
-        butt.style.color = 'grey'
-    }
-    else butt.style.color = 'red'
-}
-  */
-
-// counting likes - in consola numara in site nu / to fix
-
-  /*  let count = 0;
-    function myFunction() {
-    count+=1;
-    document.getElementById("counts").innerHTML = count;
-    console.log(count)
-      }
-*/
 const likeBtn = document.querySelectorAll(".like__btn");
 likeBtn.forEach(function (item) {
     item.addEventListener("click", (event) => {
