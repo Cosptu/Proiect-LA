53c111fc192b93f6851f3f51f59a907d200d0254
Costel Spinu <cosptu@gmail.com>
2021-09-22 20:07:03 +0300

    validation

--- proiect.js
+++ proiect.js
@@ -15,3 +15,32 @@ likeBtn.forEach(function (item) {
         }
     });
 });
+
+
+/* function validation form adopt */
+
+function validation(){
+    /* Getting value from the inputs */
+    var inputName = document.getElementById("inputName").value;
+    var inputPhone = document.getElementById("inputPhone").value;
+    var inputAddress = document.getElementById("inputAddress").value;
+    var inputCity = document.getElementById("inputCity").value;
+    var inputState = document.getElementById("inputState").value;
+    var inputZip = document.getElementById("inputZip").value;
+
+
+    /* checking if all inputs are filled */
+    var allTrue = true;
+    if (inputName.length > 1 && inputPhone.length > 1 && inputAddress.length > 1 && inputCity.length > 1 && inputState.length > 1 && inputZip.length > 1){
+        allTrue = false;
+    }
+    if (allTrue === false){
+        document.getElementById("submit").removeAttribute("disabled");
+    } else{
+        document.getElementById("submit").setAttribute("disabled","true");
+    }
+
+}
+
+
+
